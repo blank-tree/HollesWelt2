b104ef312fab74b604fd976082bcff26c3af1d1a
Joël Gähwiler <joel.gaehwiler@gmail.com>
2016-12-14 12:03:23 +0100

    added comments

--- src/Pillow.cpp
+++ src/Pillow.cpp
@@ -3,10 +3,13 @@
 void Pillow::setup() {
     static string prefix = "cu.usbmodem";
 
+    // print device list
     serial.listDevices();
-    
+
+    // get devices
     vector <ofSerialDeviceInfo> deviceList = serial.getDeviceList();
-    
+
+    // look for a device that sounds like an arduino
     for(ofSerialDeviceInfo d: deviceList) {
         if(d.getDeviceName().compare(0, prefix.size(), prefix) == 0) {
             serial.setup(d.getDeviceID(), 9600);
@@ -21,41 +24,50 @@ void Pillow::setup() {
 string Pillow::readString() {
     static string str;
     stringstream ss;
-    char ch;
-    int ttl=1000;
-    
-    while ((ch=serial.readByte()) > 0 && ttl > 0 && ch != '\n') {
+    int ch;
+
+    // read until zero
+    while ((ch=serial.readByte()) > 0 && ch != '\n') {
         ss << ch;
     }
-    
+
+    // get string
     str += ss.str();
-    
+
+    // finish on newline
     if (ch=='\n') {
         string tmp=str;
         str="";
         return tmp;
     }
-    
+
+    // otherwise return empty string
     return "";
 }
 
 void Pillow::update() {
+    // skip if not connected
     if(!connected) {
         return;
     }
-    
+
+    // read string
     string str = readString();
-    
+
+    // return if empty
     if(str == "") {
         return;
     }
-    
+
+    // split on comma
     vector<string> values = ofSplitString(str, ",");
-    
+
+    // check list size
     if(values.size() != 5) {
         return;
     }
-    
+
+    // read and set values
     for(int i=0; i<4; i++) {
         int v = ofToInt(values[i]);
         
